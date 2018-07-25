42e14abd1e108d4d0e919cc1150ce7ed23cbe211
Alan Foster <alan@alanfoster.me>
2018-07-25 23:04:45 +0100

    Add basic SQL toy parsing

--- readme.md
+++ readme.md
@@ -1,6 +1,11 @@
-# Sql In Haskell
+# SQL In Haskell
 
-> http://katychuang.com/cabal-guide/
+This repository represents a toy parser using the Parser Combinator library Parsec.
+Currently the implementation echos back the SQL that you entered.
+
+## Usage
+
+This project uses Cabal:
 
 ```
 cabal sandbox init
@@ -8,6 +13,7 @@ cabal sandbox init
 cabal configure
 cabal install
 cabal build
-cabal run
+cabal run "select func(func(*), bar, baz), qux, * from table"
 ```
 
+> http://katychuang.com/cabal-guide/
--- src/Main.hs
+++ src/Main.hs
@@ -1,16 +1,104 @@
 module Main where
 import System.Environment
 import Text.ParserCombinators.Parsec hiding (spaces)
+import Control.Monad
 
-symbol :: Parser Char
-symbol = oneOf "!$%&|*+-/:<=?>@^_~#"
+-- Parser for a simple SQL like language:
+--     select * from table
+--     select a, b, c from table
+--     select a from table
+--     select func(*), func(*) from table
+--     select func(func(*)), func(*) from table
+--     select [expr]* from table
+
+joinWithCommas :: (Show a) => [a] -> String
+joinWithCommas [] = ""
+joinWithCommas [x] = show x
+joinWithCommas (x:xs) = (show x) ++ ", " ++ (joinWithCommas xs)
+
+data Argument =
+    Star
+    | Column String
+    | Function String [Argument]
+
+instance Show Argument where
+    show (Column column) = column
+    show Star = "*"
+    show (Function name args) = name ++ "(" ++ joinWithCommas(args) ++ ")"
+
+data Table =
+    Table String
+
+instance Show Table where
+    show (Table table) = table
+
+data Sql =
+    Select [Argument] Table
+
+instance Show Sql where
+    show (Select selection table) =
+        "select " ++ (joinWithCommas selection) ++ " from " ++ (show table)
+
+spaces :: Parser ()
+spaces = skipMany1 space
+
+commaSep :: Parser a -> Parser [a]
+commaSep p = sepBy p (char ',' >> skipMany spaces)
+
+parseName :: Parser String
+parseName = many1 letter
+
+parseStar :: Parser Argument
+parseStar = char '*' >> return Star
+
+parseColumn :: Parser Argument
+parseColumn =
+    do
+        name <- parseName
+        return $ Column name
+
+parseFunc :: Parser Argument
+parseFunc =
+    do
+        name <- parseName
+        char '('
+        args <- parseArgs
+        char ')'
+        return $ Function name args
+
+parseArg :: Parser Argument
+parseArg =
+    parseStar <|>
+    try parseFunc <|>
+    parseColumn
+
+parseArgs :: Parser [Argument]
+parseArgs = commaSep parseArg
+
+parseSelect :: Parser Sql
+parseSelect =
+    do
+        string "select"
+        spaces
+        args <- parseArgs
+        spaces
+        string "from"
+        spaces
+        table <- parseName
+        return (Select args (Table table))
+
+parseExpr :: Parser Sql
+parseExpr =
+    parseSelect
 
 readExpr :: String -> String
 readExpr input =
-    case parse symbol "lisp" input of
+    case parse parseExpr "sql" input of
         Left err -> "No match: " ++ show err
-        Right val -> "Found Value"
+        Right val -> show val
 
 main :: IO ()
-main = do args <- getArgs
-          putStrLn (readExpr (args !! 0))
+main =
+    do
+        args <- getArgs
+        print $ readExpr (args !! 0)
