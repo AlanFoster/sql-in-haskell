75406091bae8557f6f2ba781cbb1833176411fce
Alan Foster <alan@alanfoster.me>
2018-07-29 22:10:15 +0100

    Clarify test helper function usage

--- src/Spec.hs
+++ src/Spec.hs
@@ -2,20 +2,23 @@ import Test.Hspec
 import Control.Exception (evaluate)
 import Parser
 
-parsesAndReturns sql = readExpr sql `shouldBe` sql
+-- Assert that the given SQL parses correctly, and that the same
+-- parsed result is returned
+assertSuccessfulParse :: String -> Assertion
+assertSuccessfulParse sql = readExpr sql `shouldBe` sql
 
 main :: IO ()
 main = hspec $ do
     describe "Parsing and printing" $ do
         describe "Simple expresions" $ do
             it "supports select statements" $ do
-                parsesAndReturns "select * from table"
+                assertSuccessfulParse "select * from table"
 
             it "supports select statements with multiple selections" $ do
-                parsesAndReturns "select table.foo, table.baz, table.qux from table"
+                assertSuccessfulParse "select table.foo, table.baz, table.qux from table"
 
             it "supports function calls" $ do
-                parsesAndReturns "select count(*) from table"
+                assertSuccessfulParse "select count(*) from table"
 
             it "supports function calls with multiple arguments" $ do
-                parsesAndReturns "select foo(a, b, c), bar(d, e, f) from table"
+                assertSuccessfulParse "select foo(a, b, c), bar(d, e, f) from table"
